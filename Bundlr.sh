433e5fd90d0ba368845e79d5ecee7bc16dabc4a6
Artem // Kallen <82199337+Kallen-c@users.noreply.github.com>
2022-07-14 19:57:28 +0400

    Update Bundlr.sh

--- Bundlr.sh
+++ Bundlr.sh
@@ -19,14 +19,7 @@ PS3='Select an action: '
 options=("Install Node" "Request tokens on website" "Run docker" "Check log" "Check balance" "Run validator" "Helpful commands" "Exit")
 select opt in "${options[@]}"
                do
-                   case $opt in
-                   
-"Check log")                 
-cd $HOME/bundlr/validator-rust && \
-docker-compose logs -f --tail 10
-
-break
-;;                   
+                   case $opt in                           
 
 "Install Node")
 echo "============================================================"
@@ -38,52 +31,50 @@ sudo apt-get update && sudo apt-get upgrade -y
 
 sudo apt-get install curl wget jq libpq-dev libssl-dev \
 build-essential pkg-config openssl ocl-icd-opencl-dev \
-libopencl-clang-dev libgomp1 -y \
+libopencl-clang-dev libgomp1 -y
 apt install docker-compose
 install() {
 	cd
 	if ! docker --version; then
 		echo -e "${C_LGn}Docker installation...${RES}"
-		sudo apt update \
-		sudo apt upgrade -y \
-		sudo apt install curl apt-transport-https ca-certificates gnupg lsb-release -y \
-		. /etc/*-release \
-		wget -qO- "https://download.docker.com/linux/${DISTRIB_ID,,}/gpg" | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg \
-		echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null \
-		sudo apt update \
-		sudo apt install docker-ce docker-ce-cli containerd.io -y \
-		docker_version=`apt-cache madison docker-ce | grep -oPm1 "(?<=docker-ce \| )([^_]+)(?= \| https)"` \
+		sudo apt update && sudo apt upgrade -y
+		sudo apt install curl apt-transport-https ca-certificates gnupg lsb-release -y
+		. /etc/*-release
+		wget -qO- "https://download.docker.com/linux/${DISTRIB_ID,,}/gpg" | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+		echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+		sudo apt update
+		sudo apt install docker-ce docker-ce-cli containerd.io -y
+		docker_version=`apt-cache madison docker-ce | grep -oPm1 "(?<=docker-ce \| )([^_]+)(?= \| https)"`
 		sudo apt install docker-ce="$docker_version" docker-ce-cli="$docker_version" containerd.io -y
 	fi
 	if ! docker-compose --version; then
 		echo -e "${C_LGn}Docker Сompose installation...${RES}"
-		sudo apt update \
-		sudo apt upgrade -y \
-		sudo apt install wget jq -y \
-		local docker_compose_version=`wget -qO- https://api.github.com/repos/docker/compose/releases/latest | jq -r ".tag_name"` \
-		sudo wget -O /usr/bin/docker-compose "https://github.com/docker/compose/releases/download/${docker_compose_version}/docker-compose-`uname -s`-`uname -m`" \
-		sudo chmod +x /usr/bin/docker-compose \
+		sudo apt update && sudo apt upgrade -y
+		sudo apt install wget jq -y
+		local docker_compose_version=`wget -qO- https://api.github.com/repos/docker/compose/releases/latest | jq -r ".tag_name"`
+		sudo wget -O /usr/bin/docker-compose "https://github.com/docker/compose/releases/download/${docker_compose_version}/docker-compose-`uname -s`-`uname -m`"
+		sudo chmod +x /usr/bin/docker-compose
 		. $HOME/.bash_profile
 	fi
 	if [ "$dive" = "true" ] && ! dpkg -s dive | grep -q "ok installed"; then
 		echo -e "${C_LGn}Dive installation...${RES}"
-		wget https://github.com/wagoodman/dive/releases/download/v0.9.2/dive_0.9.2_linux_amd64.deb \
-		sudo apt install ./dive_0.9.2_linux_amd64.deb \
+		wget https://github.com/wagoodman/dive/releases/download/v0.9.2/dive_0.9.2_linux_amd64.deb
+		sudo apt install ./dive_0.9.2_linux_amd64.deb
 		rm -rf dive_0.9.2_linux_amd64.deb
 	fi
 }
 uninstall() {
 	echo -e "${C_LGn}Docker uninstalling...${RES}"
-	sudo dpkg -r dive \
-	sudo systemctl stop docker.service docker.socket \
-	sudo systemctl disable docker.service docker.socket \
-	sudo rm -rf `systemctl cat docker.service | grep -oPm1 "(?<=^#)([^%]+)"` `systemctl cat docker.socket | grep -oPm1 "(?<=^#)([^%]+)"` /usr/bin/docker-compose \
-	sudo apt purge docker-engine docker docker.io docker-ce docker-ce-cli -y \
-	sudo apt autoremove --purge docker-engine docker docker.io docker-ce -y \
-	sudo apt autoclean \
-	sudo rm -rf /var/lib/docker /etc/appasudo rmor.d/docker \
-	sudo groupdel docker \
-	sudo rm -rf /etc/docker /usr/bin/docker /usr/libexec/docker /usr/libexec/docker/cli-plugins/docker-buildx /usr/libexec/docker/cli-plugins/docker-scan /usr/libexec/docker/cli-plugins/docker-app /usr/share/keyrings/docker-archive-keyring.gpg 
+	sudo dpkg -r dive
+	sudo systemctl stop docker.service docker.socket
+	sudo systemctl disable docker.service docker.socket
+	sudo rm -rf `systemctl cat docker.service | grep -oPm1 "(?<=^#)([^%]+)"` `systemctl cat docker.socket | grep -oPm1 "(?<=^#)([^%]+)"` /usr/bin/docker-compose
+	sudo apt purge docker-engine docker docker.io docker-ce docker-ce-cli -y
+	sudo apt autoremove --purge docker-engine docker docker.io docker-ce -y
+	sudo apt autoclean
+	sudo rm -rf /var/lib/docker /etc/appasudo rmor.d/docker
+	sudo groupdel docker
+	sudo rm -rf /etc/docker /usr/bin/docker /usr/libexec/docker /usr/libexec/docker/cli-plugins/docker-buildx /usr/libexec/docker/cli-plugins/docker-scan /usr/libexec/docker/cli-plugins/docker-app /usr/share/keyrings/docker-archive-keyring.gpg
 }
 
 # Actions
@@ -103,8 +94,7 @@ sudo apt-get install nodejs -y && \
 echo -e "\nnodejs > $(node --version).\nnpm  >>> v$(npm --version).\n"
 
 # Create Bundlr catalog
-mkdir $HOME/bundlr \
-cd $HOME/bundlr
+mkdir $HOME/bundlr; cd $HOME/bundlr
 
 # Clone repository
 git clone \
@@ -116,6 +106,7 @@ cargo run --bin wallet-tool create > wallet.json
 
 break
 ;;
+
 
 "Request tokens on website")
 
@@ -133,12 +124,20 @@ echo "==========================================================================
 break
 ;;
 
+"Check log")                 
+cd $HOME/bundlr/validator-rust && \
+docker-compose logs -f --tail 10
+
+break
+;;   
+
 "Run docker")
 PORT=2109
 echo "============================================================"
 echo "Enter your wallet address"
 echo "============================================================"
 read ADDRESS
+echo export PORT=${PORT} >> $HOME/.bash_profile
 echo export ADDRESS=${ADDRESS} >> $HOME/.bash_profile
 echo export BUNDLR_PORT=${PORT} >> $HOME/.bash_profile
 source $HOME/.bash_profile
